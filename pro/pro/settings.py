7d215d7244d3c8f94c0eb8c83bad82437b4c984d
Emmanuel <phevab1@gmail.com>
2023-10-23 13:00:56 +0000

    nmmm

--- pro/pro/settings.py
+++ pro/pro/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-(3z(gpat8(10mf9uju_o7cm-2h@_csnj42v)42x22@2n6kltfp'
-
+import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# settings.py
+
+
+
+# ...
+
+# Define the STATIC_ROOT setting
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
